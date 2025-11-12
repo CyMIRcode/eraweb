b115159bfbca2b841c3a814925a2277e0ab0b2d4
chris <148228792+CyMIRcode@users.noreply.github.com>
2025-11-12 15:28:33 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const themeConfig: ThemeConfig = {
     // site description
     description: '夜航船静态博客',
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
-    i18nTitle: true, // true, false
+    i18nTitle: false, // true, false
     // author name
     author: 'Peter',
     // site url
